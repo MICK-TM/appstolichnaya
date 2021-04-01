5e7be1262ac5d0156006fd1f8201ae98eb7ada34
v.osenchuk <semdelion@gmail.com>
2021-04-01 12:26:09 +0700

    update nugets

--- stolichnaya.Core/CoreApp.cs
+++ stolichnaya.Core/CoreApp.cs
@@ -18,7 +18,7 @@ namespace stolichnaya.Core
             get
             {
                 if (UserContext.CurrentUser.IsAuth && UserContext.CurrentUser.IfExistCards)
-                    return typeof(ProfileCardViewModel);
+                    return typeof(ProfileCardViewModelOld);
                 return typeof(OffersViewModel);
             }
         }
@@ -54,46 +54,3 @@ namespace stolichnaya.Core
          }
      }
  } 
-   /* using Loymax.Core.Services.Interfaces;
-    using Loymax.Core.ViewModels.Locators;
-    using Loymax.Module.Offers.ViewModels;
-    using Loymax.Module.Profile.ViewModels;
-    using Loymax.Module.PurchaseHistory.ViewModels;
-    using MvvmCross;
-    using MvvmCross.IoC;
-    using stolichnaya.Core.ViewModels;
-
-    public class CoreApp : App
-    {
-        /* public override Type MainViewModelType => typeof(Loymax.Module.Profile.ViewModels.ProfileCardViewModel); */
-
-
-       /* protected override ICurrentUserContext UserContext => Mvx.IoCProvider.GetSingleton<ICurrentUserContext>();
-
-        public override Type MainViewModelType
-        {
-            get 
-            {
-                if (UserContext.CurrentUser.IsAuth && UserContext.CurrentUser.IfExistCards)
-                    return typeof(ProfileAttachCardViewModel);
-                return typeof(ProfileCardViewModel);
-            } 
-    }
-
-    public override void LoadModules(ILxModuleManager moduleManager)
-    {
-        base.LoadModules(moduleManager);
-        moduleManager.EnsurePluginLoaded<Loymax.Module.Merchants.MerchantsModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.ShoppingList.ShoppingListModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.Notifications.NotificationsModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.PurchaseHistory.PurchaseHistoryModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.SignIn.SignInModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.SignUp.SignUpModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.ResetPassword.ResetPasswordModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.Offers.OffersModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.Profile.ProfileModule>();
-        moduleManager.EnsurePluginLoaded<Loymax.Module.SupportService.SupportServiceModule>();
-
-    }
-}
-}*/
--- stolichnaya.Droid/Views/NewProfileCardsView.cs
+++ stolichnaya.Droid/Views/NewProfileCardsView.cs
@@ -1,14 +1,12 @@
 ﻿
 using Android.Runtime;
-using Loymax.Module.Profile.ViewModels;
-using Loymax.Module.Profile.Droid;
 using Loymax.Module.Profile.Droid.Views;
 
 
 namespace alfa.Droid.Views
 {
     [Register(nameof(NewProfileCardsView))]
-    public class NewProfileCardsView : ProfileCardFragment
+    public class NewProfileCardsView : ProfileCardFragmentOld
     {
         protected override int FragmentId => stolichnaya.Droid.Resource.Layout.NewProfileCardsView;
     }
